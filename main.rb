57eadc6ae4b770d8c59709818bdd81d04da530dd
alt9800 <51439199+alt9800@users.noreply.github.com>
2020-10-01 15:02:58 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,22 +5,21 @@ require 'yaml'
 require "csv"
 
 
-# display mentions
 def mentionTimeline
   @client.mentions_timeline.each do |tweet|
-		if tweet.is_a?(Twitter::Tweet)
-			word = tweet.text.split(" ")[1]
-			CSV.foreach("/Users/sat/dev/ruby/reply-twitter/src/list.csv") do |row|
-				if row[1] == word
-					comment = "#{row[1]}の出現時期は#{row[2]}、#{row[3]}で#{row[4]}にみられます。\n#{row[5]}ベルで売れます。"
-					@client.update("@#{tweet.user.screen_name}\n#{comment}", options = {:in_reply_to_status_id => tweet.id})
-				end 
-			end
-		end
+    if tweet.is_a?(Twitter::Tweet)
+      word = tweet.text.split(" ")[1]
+      CSV.foreach("./list.csv") do |row|
+	if row[1] == word
+	  comment = "#{row[1]}の出現時期は#{row[2]}、#{row[3]}で#{row[4]}にみられます。\n#{row[5]}ベルで売れます。"
+	  @client.update("@#{tweet.user.screen_name}\n#{comment}", options = {:in_reply_to_status_id => tweet.id})
+	end 
+      end
+    end
   end
 end
 
-
+# yamlから自分のキーを読み込んでくる
 CONFIG = YAML.load_file('config.yaml')
 
 user_id = CONFIG["user_id"]
@@ -37,12 +36,15 @@ access_token_secret = CONFIG["access_token_secret"]
   config.access_token    = access_token
   config.access_token_secret = access_token_secret
 end
-  
+
+=begin
+# いつかストリームをうまく使ってタイムライン監視をしたい
 stream_client = Twitter::Streaming::Client.new do |config|
   config.consumer_key    = consumer_key
   config.consumer_secret   = consumer_secret
   config.access_token    = access_token
   config.access_token_secret = access_token_secret
 end
+=end
 
 mentionTimeline
